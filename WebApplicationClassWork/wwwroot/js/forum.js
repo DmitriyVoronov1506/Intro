fc1ab37beb213ffbc8c1547a0c88e7e9f56b02eb
DmitriyVoronov1506 <dmitriy.voronov1506@gmail.com>
2022-08-10 20:51:51 +0300

    Class work 10 08 22

--- WebApplicationClassWork/wwwroot/js/forum.js
+++ WebApplicationClassWork/wwwroot/js/forum.js
@@ -35,21 +35,53 @@ function loadTopics(elem) {
 
 function showTopics(elem, j) {
 
-    // шапка таблицы
-    let tableheader = "<tr><th>Number</th><th>Title</th><th>Description</th></tr>";
+    //// шапка таблицы
+    //let tableheader = "<tr><th>Number</th><th>Title</th><th>Description</th></tr>";
 
-    let table = "";
+    //let table = "";
 
-    // счётчик 
-    let i = 0;
+    //// счётчик 
+    //let i = 0;
 
-    for (let topic of j) { // бежим по топикам
+    //for (let topic of j) { // бежим по топикам
         
-        ++i; // увеличиваем счётчик
-        table += `<tr data-id='${topic.id}'><td>${i}</td><td>${topic.title}</td><td>${topic.description}</td></tr>`;
-    }
+    //    ++i; // увеличиваем счётчик
+    //    table += `<tr data-id='${topic.id}'><td>${i}</td><td>${topic.title}</td><td>${topic.description}</td></tr>`;
+    //}
 
-    // записываем в elem
-    elem.innerHTML = `<table id='topics'>${tableheader}${table}</table>`;
+    //// записываем в elem
+    //elem.innerHTML = `<table id='topics'><col style=\"width: 10px\">${tableheader}${table}</table>`;
 
+
+
+    //var trTemplate = "<tr><td>*title</td><td>*descr</td></tr>";
+    //var appHtml = "<table border=1>";
+    //for (let topic of j) {
+    //    appHtml +=
+    //        trTemplate
+    //            .replace("*title", topic.title)
+    //            .replace("*descr", topic.description);
+    //}
+    //appHtml += "</table>";
+    //elem.innerHTML = appHtml;
+
+
+    fetch("/templates/topic.html")
+        .then(r => r.text())
+        .then(trTemplate => {
+
+            var appHtml = "";
+
+            for (let topic of j) {
+                appHtml +=
+                    trTemplate
+                        .replace("{{title}}", topic.title)
+                        .replace("{{description}}", topic.description)
+                        .replace("{{id}}", topic.id);
+            }
+
+            elem.innerHTML = appHtml;
+
+        });
+ 
 }
